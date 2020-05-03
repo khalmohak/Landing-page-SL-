c3859afa967f189d73c59016af8ae50db4d80c7f
varunvj1 <varunvj181@gmail.com>
2020-05-03 20:54:21 +0530

    Some minor changes

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-  myFunction();
-};
+// var started = document.getElementById("getStarted");
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// function inputParallax() {
+//   started.style.border = "none";
+//   started.style.borderBottom = "1px solid red";
+// }
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function inputParallaxBlur() {
+//   started.style.borderBottom = "1px solid white";
+// }
